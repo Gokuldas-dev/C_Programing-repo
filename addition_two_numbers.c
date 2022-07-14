a7a5bb9704053ae634b593a5e33bcedf604ae9dd
Gokuldas Shenoy.K <74420066+Gokuldas-dev@users.noreply.github.com>
2022-07-14 15:53:02 +0530

    Addition of two numbers

---
+++ addition_two_numbers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int num1, num2,sum;
+    printf("Enter Two Numbers : ");
+    scanf("%d%d",&num1,&num2);
+    sum = num1 + num2;
+    printf("Total Sum is : %d",sum);
+    return 0;
+}

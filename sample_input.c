e090e4606ee4d709a6cae459384b741bd27bb54d
Gokuldas Shenoy.K <74420066+Gokuldas-dev@users.noreply.github.com>
2022-07-14 16:57:19 +0530

    Created sample_input.c

---
+++ sample_input.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+  int a ;
+  printf("Enter a Number : ");
+  scanf("%d",&a);
+  printf("You have entered a number : %d",a);
+
+  return 0;
+  
+}

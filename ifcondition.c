8dc7b1ff886ff3bdaf19b475dad97824da99260a
Gokuldas Shenoy.K <74420066+Gokuldas-dev@users.noreply.github.com>
2022-07-14 20:39:11 +0530

    ifstatement.c

---
+++ ifcondition.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main()
+{
+
+  int a ;
+  printf("Enter a Number :");
+  scanf("%d",&a);
+  if(a<0)
+  {
+    printf("Number is Negative");
+
+  }
+  else
+  {
+    printf("NUmber is Positive");
+  }
+
+  return 0;
+}

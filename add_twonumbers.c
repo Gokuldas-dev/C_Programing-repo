32dba6a1a594862a552439c06f64a328f6057713
Gokuldas Shenoy.K <74420066+Gokuldas-dev@users.noreply.github.com>
2022-07-15 17:24:52 +0530

    float and integer number operation

---
+++ add_twonumbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main()
+{
+  int num1,num2,sum;
+  printf("Enter two numbers :");
+  scanf("%d%d",&num1,&num2);
+  sum = num1 + num2;
+  printf("Answer is %d",sum);
+
+  return 0;
+}
---
+++ multi_flota_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int main()
+
+{
+
+float num1,num2,ans;
+printf("Enter two floating point number : ");
+scanf("%f%f",&num1,&num2);
+ans = num1 * num2;
+printf("Sum  of Two Numbers is : %f",ans);
+return 0;
+}

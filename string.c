c8709cbce8bef97a93cd16822909a8314ffbdad6
Gokuldas Shenoy.K <74420066+Gokuldas-dev@users.noreply.github.com>
2022-07-19 08:37:32 +0530

    String insert using array

---
+++ string.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+
+  char name[10];
+  printf("Enter Your Name : ");
+  scanf("%s",name);
+  printf("You Entred Name is : %s",name);
+
+  return 0;
+}
